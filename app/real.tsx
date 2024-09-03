474ec86d3baf12d5aa804dcfa24e66da2637b2e6
Gavin Bates <chessinato@gmail.com>
2024-09-03 10:17:35 -0400

    Change

--- app/real.tsx
+++ app/real.tsx
@@ -17,8 +17,6 @@ export default function () {
   let [groups, setGroups] = useState(1);
 
   function getRandomGroups(members: string[]) {
-    members = members.filter(Boolean);
-
     function shuffleArray(array: string[]) {
       for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -88,10 +86,17 @@ export default function () {
                             >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
                                 viewBox="0 0 24 24"
-                                fill="currentColor"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-fit"
                               >
-                                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M12 4.5v15m7.5-7.5h-15"
+                                />
                               </svg>
                             </Button>
                             <Modal open={state} closeModal={update}>
@@ -106,6 +111,8 @@ export default function () {
                                     data.get("name") as string,
                                     (data.get("members") as string).split("\n"),
                                   ];
+
+                                  members = members.filter(Boolean);
 
                                   setActivePreset([newName, members]);
                                   setPresets((presets) => {
@@ -180,6 +187,7 @@ export default function () {
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
                           fill="currentColor"
+                          className="w-fit"
                         >
                           <path
                             fillRule="evenodd"
@@ -208,6 +216,7 @@ export default function () {
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
+                    className="w-fit"
                   >
                     <path
                       strokeLinecap="round"
@@ -228,6 +237,8 @@ export default function () {
                         data.get("name") as string,
                         (data.get("members") as string).split("\n"),
                       ];
+
+                      members = members.filter(Boolean);
 
                       setActivePreset([name, members]);
                       setPresets((presets) => [...presets, [name, members]]);
@@ -276,7 +287,7 @@ export default function () {
           />
           {activePreset && (
             <div className="flex flex-wrap gap-4 m-4">
-              {getRandomGroups(activePreset[1]).map((group, index) => (
+              {[...getRandomGroups(activePreset[1])].map((group, index) => (
                 <Paper key={index} className="p-4 w-40 overflow-hidden">
                   <p
                     className="text-center"
